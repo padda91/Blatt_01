ebfb76f64006af5c154d55e26a6b68168a57c06a
padda <padda@DESKTOP-FQ7H9NR>
2019-09-02 14:48:02 +0200

    Second Commit

--- Blatt_01/src/aufg_04/Main_014_7351510.java
+++ Blatt_01/src/aufg_04/Main_014_7351510.java
@@ -4,8 +4,8 @@ public class Main_014_7351510 {
 
 	public static void main(String[] args) {
 		
-		int i=5;
-		int j=5; //Zeile A
+		int i=4;
+		int j=6; //Zeile A
 		System.out.println(i+j); //Zeile B
 		
 		}
