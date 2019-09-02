10bf5be14112c3543674527ac23e967da7aa4f31
padda <padda@DESKTOP-FQ7H9NR>
2019-09-02 14:46:03 +0200

    First Commit

---
+++ Blatt_01/src/aufg_04/Main_014_7351510.java
@@ -0,0 +1,15 @@
+package aufg_04;
+
+public class Main_014_7351510 {
+
+	public static void main(String[] args) {
+		
+		int i=5;
+		int j=5; //Zeile A
+		System.out.println(i+j); //Zeile B
+		
+		}
+
+	}
+
+
---
+++ Blatt_01/src/aufg_05/Main_015_7351510.java
@@ -0,0 +1,15 @@
+package aufg_05;
+
+public class Main_015_7351510 {
+
+	public static void main(String[] args) {
+
+		
+		int i=5;
+		int j=5; //Zeile A
+		System.out.println("i+j"); //Zeile B
+		
+		}
+
+
+}
